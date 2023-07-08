04f30baaea8408fb4fc0e4e8bcc58b9c2798d126
Bestis21 <zaichkodmytro2004@gmail.com>
2023-07-08 13:48:39 +0200

    crt smth

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nodejs-cli-app
+https://monosnap.com/file/LOnReiDwrxeRvCvxkDEWwHizbkBu5L
+
+https://monosnap.com/file/947rTqyIYpHmaFL3EvZQR8jYxrk54d
---
+++ index.js
@@ -0,0 +1,37 @@
+// const sb = require('./contacts')
+// // console.log(sb)
+// const { vyna } = require('./contacts')
+// console.log(vyna)
+const contacts = require('./contacts')
+const argv = require('yargs').argv
+
+const  invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+      case 'list':
+          const list = await contacts.listContacts();
+          console.table(list)
+          break;
+    case 'get':
+          const contactById = await contacts.getContactById(id);
+          if (!contactById) {
+              throw new Error(`Contact with id ${id} not found`);
+          }
+          console.table(contactById)
+      break;
+
+    case 'add':
+    const newContact = contacts.addContact({name, email, phone})  
+     console.table(newContact)
+      break;
+
+    case 'remove':
+      const deleteContacts = await contacts.removeContact(id)
+      console.table(deleteContacts)
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv)
